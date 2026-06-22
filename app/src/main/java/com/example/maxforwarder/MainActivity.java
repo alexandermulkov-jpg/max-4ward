0490db75c5a576bed492dd23b6168537e2ef9427
ASD_36 <alexandermulkov@gmail.com>
2026-06-22 14:41:15 +0500

    Update MainActivity.java

--- app/src/main/java/com/example/maxforwarder/MainActivity.java
+++ app/src/main/java/com/example/maxforwarder/MainActivity.java
@@ -1,19 +1,20 @@
 package com.example.maxforwarder;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
+        // Создаем кнопку стандартными средствами чистого Android
         Button btn = new Button(this);
         btn.setText("Дать разрешение на чтение уведомлений");
         setContentView(btn);
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Открываем системный экран разрешений к пушам
                 Intent intent = new Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS);
                 startActivity(intent);
                 Toast.makeText(MainActivity.this, "Найдите в списке 'MAX To TG Forwarder' и включите его", Toast.LENGTH_LONG).show();
