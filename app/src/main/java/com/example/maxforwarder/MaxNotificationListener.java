f92270a2586c11b2d3689c15aa4a0bc92deb866c
ASD_36 <alexandermulkov@gmail.com>
2026-06-23 11:06:34 +0500

    Update MaxNotificationListener.java

--- app/src/main/java/com/example/maxforwarder/MaxNotificationListener.java
+++ app/src/main/java/com/example/maxforwarder/MaxNotificationListener.java
@@ -37,7 +37,7 @@ public class MaxNotificationListener extends NotificationListenerService {
 
     @Override
     public void onNotificationPosted(StatusBarNotification sbn) {
-        if ("ru.oneme.app".equals(sbn.getPackageName())) {
+        if (true) {
             Notification notification = sbn.getNotification();
             Bundle extras = notification.extras;
 
